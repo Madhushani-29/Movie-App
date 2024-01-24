386a5dc6a52c271b6778352e2165f714c37c6bee
madhushani-29 <madhushani2904@gmail.com>
2024-01-24 21:35:21 +0530

    movie entity created

---
+++ lib/domain/entities/Movie.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class Movie {
+  final int id;
+  final String title;
+  final String overview;
+  final String posterPath;
+  Movie({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+  });
+}
