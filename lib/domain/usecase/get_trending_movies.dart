b3fa3ac09ad6222c5031e089ffe3e5f04fbb2aab
madhushani-29 <madhushani2904@gmail.com>
2024-01-25 17:33:26 +0530

    test

--- lib/domain/usecase/get_trending_movies.dart
+++ lib/domain/usecase/get_trending_movies.dart
@@ -4,9 +4,9 @@ import 'package:movieapp/domain/repositories/movie_repository.dart';
 
 class GetTrendingMovies {
   final MovieRepository repository;
-  GetTrendingMovies({
-    required this.repository,
-  });
+  GetTrendingMovies(
+    this.repository,
+  );
 
   Future <List<Movie>> call() async {
     return await repository.getTrendingMovies();
