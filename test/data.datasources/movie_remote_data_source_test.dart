127c6e8bbe2d171dfee31c0d7eae0918e55ea3d0
madhushani-29 <madhushani2904@gmail.com>
2024-01-26 17:23:15 +0530

    datasource test

--- test/data.datasources/movie_remote_data_source_test.dart
+++ test/data.datasources/movie_remote_data_source_test.dart
@@ -1,4 +1,28 @@
+import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
-@GenerateNiceMocks([MockSpec<http.Client>()])
-void main(){}
+import 'package:mockito/mockito.dart';
+import 'package:movieapp/data/data_source/movie_remote_data_source.dart';
+
+import 'movie_remote_data_source_test.mocks.dart';
+
+@GenerateMocks([http.Client])
+void main() {
+  late MovieRemoteDataSource dataSource;
+  late MockClient mockHttpClient;
+
+  setUp(() {
+    mockHttpClient = MockClient();
+    dataSource = MovieRemoteDataSource(client:mockHttpClient);
+  });
+
+  test('should perform a GET request on a url to get trending movies', () async {
+    // arrange
+    when(mockHttpClient.get(any))
+        .thenAnswer((_) async => http.Response('our response', 200));
+    // act
+    await dataSource.getTrendingMovies();
+    // assert
+    verify(mockHttpClient.get(Uri.parse('our api')));
+  });
+}
