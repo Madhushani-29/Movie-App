d3d6025608a72150ec1a50925ad39f56c2c81f68
madhushani-29 <madhushani2904@gmail.com>
2024-01-26 12:34:59 +0530

    popular movie get test added

---
+++ test/domain.usecases/get_popular_movies_test.dart
@@ -0,0 +1,36 @@
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:movieapp/domain/entities/Movie.dart';
+import 'package:movieapp/domain/repositories/movie_repository.dart';
+import 'package:movieapp/domain/usecase/get_popular_movies.dart';
+
+import 'get_trending_movies_test.mocks.dart';
+
+@GenerateNiceMocks([MockSpec<MovieRepository>()])
+void main(){
+  late GetPopularMovies usecase;
+  late MockMovieRepository mockMovieRepository;
+
+  setUp(() {
+    mockMovieRepository = MockMovieRepository();
+    usecase = GetPopularMovies(mockMovieRepository);
+  });
+
+  final tMoviesList = [
+    Movie(id: 1, title: "Test Movie 1", overview: "Desc 1", posterPath: "/image1"),
+    Movie(id: 2, title: "Test Movie 2", overview: "Desc 2", posterPath: "/image2"),
+  ];
+
+  test('should get trending movies from the repository', () async {
+    // arrange
+    when(mockMovieRepository.getPopularMovies())
+        .thenAnswer((_) async => tMoviesList);
+    // act
+    final result = await usecase();
+    // assert
+    expect(result, tMoviesList);
+    verify(mockMovieRepository.getPopularMovies());
+    verifyNoMoreInteractions(mockMovieRepository);
+  });
+}
