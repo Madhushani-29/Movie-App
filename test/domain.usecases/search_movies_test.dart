beac043c509deb77de4513a7063ea029aff9fe79
madhushani-29 <madhushani2904@gmail.com>
2024-01-26 14:41:02 +0530

    search movie test added

--- test/domain.usecases/search_movies_test.dart
+++ test/domain.usecases/search_movies_test.dart
@@ -3,34 +3,44 @@ import 'package:mockito/mockito.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:movieapp/domain/entities/Movie.dart';
 import 'package:movieapp/domain/repositories/movie_repository.dart';
-import 'package:movieapp/domain/usecase/get_popular_movies.dart';
+import 'package:movieapp/domain/usecase/search_movies.dart';
 
 import 'get_trending_movies_test.mocks.dart';
 
 @GenerateNiceMocks([MockSpec<MovieRepository>()])
-void main(){
-  late GetPopularMovies usecase;
+void main() {
+  late SearchMovies usecase;
   late MockMovieRepository mockMovieRepository;
 
   setUp(() {
     mockMovieRepository = MockMovieRepository();
-    usecase = GetPopularMovies(mockMovieRepository);
+    usecase = SearchMovies(mockMovieRepository);
   });
 
+  final tQuery = "Inception";
+
   final tMoviesList = [
-    Movie(id: 1, title: "Test Movie 1", overview: "Desc 1", posterPath: "/image1"),
-    Movie(id: 2, title: "Test Movie 2", overview: "Desc 2", posterPath: "/image2"),
+    Movie(
+        id: 1,
+        title: "Test Movie 1",
+        overview: "Desc 1",
+        posterPath: "/image1"),
+    Movie(
+        id: 2,
+        title: "Test Movie 2",
+        overview: "Desc 2",
+        posterPath: "/image2"),
   ];
 
-  test('should get popular movies from the repository', () async {
+  test('should get trending movies from the repository', () async {
     // arrange
-    when(mockMovieRepository.getPopularMovies())
+    when(mockMovieRepository.searchMovies(any))
         .thenAnswer((_) async => tMoviesList);
     // act
-    final result = await usecase();
+    final result = await usecase(tQuery);
     // assert
     expect(result, tMoviesList);
-    verify(mockMovieRepository.getPopularMovies());
+    verify(mockMovieRepository.searchMovies(tQuery));
     verifyNoMoreInteractions(mockMovieRepository);
   });
 }
