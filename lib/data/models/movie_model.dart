f8a091711d47e226213993465fcf7666b7fd508c
madhushani-29 <madhushani2904@gmail.com>
2024-01-26 16:23:19 +0530

    movie modal  added

---
+++ lib/data/models/movie_model.dart
@@ -0,0 +1,33 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+class MovieModel {
+  int id;
+  String title;
+  String overview;
+  String posterPath;
+
+  MovieModel({
+    required this.id,
+    required this.title,
+    required this.overview,
+    required this.posterPath,
+  });
+
+  //convert JSON to MovieModal
+  factory MovieModel.fromJson(Map<String, dynamic> json) {
+    return MovieModel(
+        id: json['id'],
+        title: json['title'],
+        overview: json['overview'],
+        posterPath: json['posterPath']);
+  }
+
+  //convert MoviModel to JSON
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'title': title,
+      'overview': overview,
+      'posterPath': posterPath,
+    };
+  }
+}
