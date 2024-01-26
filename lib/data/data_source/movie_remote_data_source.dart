6188c8fff10f5c3e58ba5d7352ff6847d1ebfc21
madhushani-29 <madhushani2904@gmail.com>
2024-01-26 21:23:28 +0530

    testing

--- lib/data/data_source/movie_remote_data_source.dart
+++ lib/data/data_source/movie_remote_data_source.dart
@@ -1,1 +1,8 @@
-class MovieRemoteDataSource{}
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:movieapp/data/models/movie_model.dart';
+
+abstract class MovieRemoteDataSource {
+  Future <List<MovieModel>> getTrendingMovies ();
+  Future <List<MovieModel>> getPopularMovies ();
+  Future <List<MovieModel>> searchMovies (String query);
+}
--- test/data.datasources/movie_remote_data_source_test.dart
+++ test/data.datasources/movie_remote_data_source_test.dart
@@ -2,7 +2,9 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
 import 'package:http/http.dart' as http;
 import 'package:mockito/mockito.dart';
+import 'package:movieapp/core/error/server_exception.dart';
 import 'package:movieapp/data/data_source/movie_remote_data_source.dart';
+import 'package:movieapp/data/data_source/remote/movie_remote_data_source_impl.dart';
 
 import 'movie_remote_data_source_test.mocks.dart';
 
@@ -13,16 +15,33 @@ void main() {
 
   setUp(() {
     mockHttpClient = MockClient();
-    dataSource = MovieRemoteDataSource(client:mockHttpClient);
+    dataSource = MovieRemoteDataSourceImpl(client: mockHttpClient);
   });
 
-  test('should perform a GET request on a url to get trending movies', () async {
+  final String tUrl =
+      "https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=26e2a7846caf53650a6f9bc938e4dc0d";
+  const String sampleAPIResponse = "'''...'''";
+
+  test('should perform a GET request on a url to get trending movies',
+      () async {
     // arrange
-    when(mockHttpClient.get(any))
-        .thenAnswer((_) async => http.Response('our response', 200));
+    when(mockHttpClient.get(Uri.parse(tUrl)))
+        .thenAnswer((_) async => http.Response(sampleAPIResponse, 200));
     // act
-    await dataSource.getTrendingMovies();
+    await dataSource.getTrendingMovies;
     // assert
     verify(mockHttpClient.get(Uri.parse('our api')));
+  });
+
+  test('should throw a ServerException when the response code is 404',
+      () async {
+    // arrange
+    when(mockHttpClient.get(Uri.parse(tUrl)))
+        .thenAnswer((_) async => http.Response(sampleAPIResponse, 400));
+    // act
+    await dataSource.getTrendingMovies;
+    final call = dataSource.getTrendingMovies;
+    // assert
+    expect(() => call(), throwsA(isA<ServerException>));
   });
 }
