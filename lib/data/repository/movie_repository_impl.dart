861f014f7457843fa99c439c712c6cb8d75828b1
madhushani-29 <madhushani2904@gmail.com>
2024-01-29 20:19:41 +0530

    movie repo implements

--- lib/data/repository/movie_repository_impl.dart
+++ lib/data/repository/movie_repository_impl.dart
@@ -1,3 +1,6 @@
+import 'package:dartz/dartz.dart';
+import 'package:movieapp/core/error/server_exception.dart';
+import 'package:movieapp/core/error/server_failure.dart';
 import 'package:movieapp/data/data_source/movie_remote_data_source.dart';
 import 'package:movieapp/data/models/movie_model.dart';
 import 'package:movieapp/domain/entities/Movie.dart';
@@ -9,29 +12,41 @@ class MovieRepositoryImpl implements MovieRepository {
 
   MovieRepositoryImpl({required this.remoteDataSource});
   @override
-  Future<List<Movie>> getPopularMovies() async {
-    final List<MovieModel> movieModels =
-        await remoteDataSource.getPopularMovies();
-    final List<Movie> movies =
-        movieModels.map((model) => model.toEntity()).toList();
-    return movies;
+  Future<Either<Failure, List<Movie>>> getPopularMovies() async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.getPopularMovies();
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<List<Movie>> getTrendingMovies() async {
-    final List<MovieModel> movieModels =
-        await remoteDataSource.getTrendingMovies();
-    final List<Movie> movies =
-        movieModels.map((model) => model.toEntity()).toList();
-    return movies;
+  Future<Either<Failure, List<Movie>>> getTrendingMovies() async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.getTrendingMovies();
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 
   @override
-  Future<List<Movie>> searchMovies(String query) async {
-    final List<MovieModel> movieModels =
-        await remoteDataSource.searchMovies(query);
-    final List<Movie> movies =
-        movieModels.map((model) => model.toEntity()).toList();
-    return movies;
+  Future<Either<Failure, List<Movie>>> searchMovies(String query) async {
+    try {
+      final List<MovieModel> movieModels =
+          await remoteDataSource.searchMovies(query);
+      final List<Movie> movies =
+          movieModels.map((model) => model.toEntity()).toList();
+      return Right(movies);
+    } on ServerException {
+      return Left(ServerFailure());
+    }
   }
 }
--- test/data/repository/movie_repository_impl_test.mocks.dart
+++ test/data/repository/movie_repository_impl_test.mocks.dart
@@ -3,11 +3,13 @@
 // Do not manually edit this file.
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'dart:async' as _i3;
+import 'dart:async' as _i4;
 
+import 'package:dartz/dartz.dart' as _i2;
 import 'package:mockito/mockito.dart' as _i1;
-import 'package:movieapp/data/data_source/movie_remote_data_source.dart' as _i2;
-import 'package:movieapp/data/models/movie_model.dart' as _i4;
+import 'package:movieapp/core/error/server_failure.dart' as _i5;
+import 'package:movieapp/data/data_source/movie_remote_data_source.dart' as _i3;
+import 'package:movieapp/domain/entities/Movie.dart' as _i6;
 
 // ignore_for_file: type=lint
 // ignore_for_file: avoid_redundant_argument_values
@@ -22,42 +24,97 @@ import 'package:movieapp/data/models/movie_model.dart' as _i4;
 // ignore_for_file: camel_case_types
 // ignore_for_file: subtype_of_sealed_class
 
+class _FakeEither_0<L, R> extends _i1.SmartFake implements _i2.Either<L, R> {
+  _FakeEither_0(
+    Object parent,
+    Invocation parentInvocation,
+  ) : super(
+          parent,
+          parentInvocation,
+        );
+}
+
 /// A class which mocks [MovieRemoteDataSource].
 ///
 /// See the documentation for Mockito's code generation for more information.
 class MockMovieRemoteDataSource extends _i1.Mock
-    implements _i2.MovieRemoteDataSource {
+    implements _i3.MovieRemoteDataSource {
   @override
-  _i3.Future<List<_i4.MovieModel>> getTrendingMovies() => (super.noSuchMethod(
+  _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>> getTrendingMovies() =>
+      (super.noSuchMethod(
         Invocation.method(
           #getTrendingMovies,
           [],
         ),
-        returnValue: _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
+        returnValue: _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+            _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #getTrendingMovies,
+            [],
+          ),
+        )),
         returnValueForMissingStub:
-            _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
-      ) as _i3.Future<List<_i4.MovieModel>>);
+            _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+                _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #getTrendingMovies,
+            [],
+          ),
+        )),
+      ) as _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>);
 
   @override
-  _i3.Future<List<_i4.MovieModel>> getPopularMovies() => (super.noSuchMethod(
+  _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>> getPopularMovies() =>
+      (super.noSuchMethod(
         Invocation.method(
           #getPopularMovies,
           [],
         ),
-        returnValue: _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
+        returnValue: _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+            _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #getPopularMovies,
+            [],
+          ),
+        )),
         returnValueForMissingStub:
-            _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
-      ) as _i3.Future<List<_i4.MovieModel>>);
+            _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+                _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #getPopularMovies,
+            [],
+          ),
+        )),
+      ) as _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>);
 
   @override
-  _i3.Future<List<_i4.MovieModel>> searchMovies(String? query) =>
+  _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>> searchMovies(
+          String? query) =>
       (super.noSuchMethod(
         Invocation.method(
           #searchMovies,
           [query],
         ),
-        returnValue: _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
+        returnValue: _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+            _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #searchMovies,
+            [query],
+          ),
+        )),
         returnValueForMissingStub:
-            _i3.Future<List<_i4.MovieModel>>.value(<_i4.MovieModel>[]),
-      ) as _i3.Future<List<_i4.MovieModel>>);
+            _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>.value(
+                _FakeEither_0<_i5.Failure, List<_i6.Movie>>(
+          this,
+          Invocation.method(
+            #searchMovies,
+            [query],
+          ),
+        )),
+      ) as _i4.Future<_i2.Either<_i5.Failure, List<_i6.Movie>>>);
 }
