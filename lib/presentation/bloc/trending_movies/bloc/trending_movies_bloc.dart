5732ffcff9f0df8feb737140c2bc1d71cf9f47a2
madhushani-29 <madhushani2904@gmail.com>
2024-02-03 22:35:25 +0530

    trending movies bloc

--- lib/presentation/bloc/trending_movies/bloc/trending_movies_bloc.dart
+++ lib/presentation/bloc/trending_movies/bloc/trending_movies_bloc.dart
@@ -3,10 +3,8 @@ import 'package:bloc/bloc.dart';
 part 'trending_movies_event.dart';
 part 'trending_movies_state.dart';
 
-class TrendingMoviesBloc extends Bloc<TrendingMoviesEvent, TrendingMoviesState> {
-  TrendingMoviesBloc() : super(TrendingMoviesInitial()) {
-    on<TrendingMoviesEvent>((event, emit) {
-      // TODO: implement event handler
-    });
-  }
+class TrendingMoviesBloc
+    extends Bloc<TrendingMoviesEvent, TrendingMoviesState> {
+  final GetTrendingMovies getTrendingMovies;
+  TrendingMoviesBloc(this.getTrendingMovies) : super(TrendingMoviesInitial()) {}
 }
