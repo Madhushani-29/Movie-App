b9b80e670583c70a758f6579b358fd6d15cf743f
madhushani-29 <madhushani2904@gmail.com>
2024-02-04 17:16:15 +0530

    injection container

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,10 +1,15 @@
 import 'package:get_it/get_it.dart';
-import 'package:http/http.dart';
+import 'package:movieapp/data/data_source/movie_remote_data_source.dart';
+import 'package:movieapp/data/data_source/remote/movie_remote_data_source_impl.dart';
 import 'package:movieapp/data/repository/movie_repository_impl.dart';
 import 'package:movieapp/domain/repositories/movie_repository.dart';
+import 'package:movieapp/domain/usecase/get_popular_movies.dart';
+import 'package:movieapp/domain/usecase/get_trending_movies.dart';
+import 'package:movieapp/domain/usecase/search_movies.dart';
 import 'package:movieapp/presentation/bloc/popular_movies/bloc/popular_movie_bloc.dart';
 import 'package:movieapp/presentation/bloc/search_movies/bloc/search_movies_bloc.dart';
 import 'package:movieapp/presentation/bloc/trending_movies/bloc/trending_movies_bloc.dart';
+import 'package:http/http.dart' as http;
 
 //Create an instance of the GetIt service locator
 final getIt = GetIt.instance;
@@ -19,10 +24,18 @@ void init() {
   //whenever someone requests an instance of PopularMovieBloc, GetIt will create a new instance
 
   //Use cases
+  getIt.registerLazySingleton(() => GetPopularMovies(getIt()));
+  getIt.registerLazySingleton(() => GetTrendingMovies(getIt()));
+  getIt.registerLazySingleton(() => SearchMovies(getIt()));
+
+  //Repositories
   getIt.registerLazySingleton<MovieRepository>(
       () => MovieRepositoryImpl(remoteDataSource: getIt()));
 
-  //Repositories
   //Data sources
+  getIt.registerLazySingleton<MovieRemoteDataSource>(
+      () => MovieRemoteDataSourceImpl(client: getIt()));
+
   //Http services
+  getIt.registerLazySingleton(() => http.Client());
 }
