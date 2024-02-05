deab7aac972f4c66680c4d3354ada00fd8be05f2
madhushani-29 <madhushani2904@gmail.com>
2024-02-05 10:42:54 +0530

    presentation layer

--- lib/presentation/pages/search_movies_screen.dart
+++ lib/presentation/pages/search_movies_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class SearchMovieScreen extends StatelessWidget {
-  const SearchMovieScreen({super.key});
+  const SearchMovieScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +14,25 @@ class SearchMovieScreen extends StatelessWidget {
           children: [
             Row(
               children: [
-                TextField(
-                  controller: searchQueryController,
-                  keyboardType: TextInputType.name,
-                  style: TextStyle(fontSize: 12),
-                  autofocus: true,
-                  cursorColor: Colors.green[400],
-                  decoration: InputDecoration(
-                    fillColor: Colors.purple[50],
-                    filled: true,
-                    prefixIcon: Icon(Icons.location_city),
-                    hintText: "Enter Name",
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(),
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(10.0),
+                Expanded(
+                  child: TextField(
+                    onChanged: (value) {
+                      searchQuery = value;
+                    },
+                    controller: searchQueryController,
+                    keyboardType: TextInputType.name,
+                    style: TextStyle(fontSize: 12),
+                    autofocus: true,
+                    cursorColor: Colors.black,
+                    decoration: InputDecoration(
+                      filled: true,
+                      prefixIcon: Icon(Icons.location_city),
+                      hintText: "Enter Name",
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide(),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(10.0),
+                        ),
                       ),
                     ),
                   ),
@@ -36,21 +40,22 @@ class SearchMovieScreen extends StatelessWidget {
                 ElevatedButton(
                   onPressed: () {
                     print("Clicked");
+                    print(searchQuery);
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: Colors.orange[200],
                     shape: RoundedRectangleBorder(
                       borderRadius: BorderRadius.circular(5),
                     ),
-                    fixedSize: Size(100, 30),
+                    fixedSize: const Size(100, 30),
                   ),
-                  child: Text(
+                  child: const Text(
                     'Search',
                     style: TextStyle(color: Colors.black),
                   ),
                 ),
               ],
-            )
+            ),
           ],
         ),
       ),
